7be76372bba460a6151df8fd2700e4cb5e864d7a
kilitary <kilitary@gmail.com>
2019-12-12 17:52:30 +0300

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
 $(function() {
     $('a').on('click', function(e) {
-        e.preventDefault();
+        e.preventDefault()
         var href = $(this).attr('href')
-        $.get(href,  function(data) {
-            $('#content').html(data);
+        $.get(href, function(data) {
+            $('#content').html(data)
         })
     })
 })
